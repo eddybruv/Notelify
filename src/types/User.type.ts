858edd58c0ddabfdc9f511ace3f805cd7c4957b3
eddybruv <edwinajong@gmail.com>
2022-06-26 21:30:34 +0100

    added user type

---
+++ src/types/User.type.ts
@@ -0,0 +1,9 @@
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  password: string;
+}
