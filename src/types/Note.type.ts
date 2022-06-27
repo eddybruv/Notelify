1b3be3c24fba7f2eac9af12a37b6a68cb4dab1df
eddybruv <edwinajong@gmail.com>
2022-06-27 19:56:54 +0100

    added more types to note

--- src/types/Note.type.ts
+++ src/types/Note.type.ts
@@ -3,4 +3,7 @@ import {Document} from "mongoose";
 export interface INote extends Document {
   header: string;
   content: string;
+  accessType: string;
+  userID: string;
+  workspaceID: string;
 }
