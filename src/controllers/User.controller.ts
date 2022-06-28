6f8d996c51154d384b463caee8714d14181654bd
eddybruv <edwinajong@gmail.com>
2022-06-28 21:04:33 +0100

    defined router functions

---
+++ src/controllers/User.controller.ts
@@ -0,0 +1,76 @@
+import {Request, Response} from "express";
+import {IUser} from "../types/User.type";
+import UserModel from "../models/User.model";
+import WorkspaceModel from "../models/Workspace.model";
+
+const createUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      name,
+      username,
+      imageUrl,
+      password,
+      email
+    } = req.body as Pick<IUser, "name" | "username" | "imageUrl" | "password" | "email">;
+
+    const user: IUser = await new UserModel({
+      name,
+      username,
+      imageUrl,
+      password,
+      email,
+      workspaceIDs: [],
+    });
+
+    const newUser: IUser = await user.save();
+    res.json({message: "new user created", data: newUser});
+
+  } catch (error) {
+    throw (error)
+  }
+}
+
+const getUsers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const users: IUser[] = await UserModel.find();
+    res.status(200).json({data: users})
+  } catch (error) {
+    throw (error);
+  }
+}
+
+const addUserToWorkspace = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {userIDs, _id} = req.body;
+    const add = await WorkspaceModel.findByIdAndUpdate({_id}, {memberIDs: [...userIDs]});
+  } catch (error) {
+    throw (error)
+  }
+}
+
+const getWorkspaceMembers = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const workspaceID = req.params.id;
+    const users: IUser[] = await UserModel.find({workspaceIDs: workspaceID})
+      .populate({path: "workspaceIDs"});
+
+    users.length !== 0 ?
+      res.json({message: "found members", data: users}) :
+      res.json({message: "no members"});
+  } catch (e) {
+    throw (e);
+  }
+}
+
+const updateOwner = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {_id, workspaceIDs} = req.body as IUser;
+    await UserModel.findOneAndUpdate({_id}, {workspaceIDs});
+    const user = UserModel.findById({_id});
+    res.status(200).json({message: "user updated", data: user})
+  } catch (e) {
+    throw(e);
+  }
+}
+
+export {getUsers, addUserToWorkspace, createUser, getWorkspaceMembers, updateOwner}
