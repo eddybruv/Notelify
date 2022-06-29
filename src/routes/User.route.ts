a9677b75999b3b7c8a269724b3bda51d66f26df6
eddybruv <edwinajong@gmail.com>
2022-06-29 20:40:31 +0100

    added login route

--- src/routes/User.route.ts
+++ src/routes/User.route.ts
@@ -1,5 +1,12 @@
 import {Router} from "express";
-import {createUser, addUserToWorkspace, getUsers, getWorkspaceMembers, updateOwner} from "../controllers/User.controller.js";
+import {
+  createUser,
+  addUserToWorkspace,
+  getUsers,
+  getWorkspaceMembers,
+  updateOwner,
+  loginUser
+} from "../controllers/User.controller.js";
 
 const router: Router = Router();
 
@@ -8,5 +15,6 @@ router.post("/add-to-workspace", addUserToWorkspace);
 router.get('/get-users', getUsers);
 router.get("/get-workspace-members/:id", getWorkspaceMembers);
 router.post("/update-owner", updateOwner);
+router.post("/login", loginUser);
 
 export default router
