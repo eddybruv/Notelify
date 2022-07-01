f4e6aadcb02e1283ce8875aaa61d7c5ba9da913d
eddybruv <edwinajong@gmail.com>
2022-07-01 22:28:29 +0100

    created component view notes

---
+++ client/src/components/Notes/ViewNotes.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from "react";
+import style from "../../styles/view.module.css";
+
+const ViewNotes:FC = () => {
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default ViewNotes
