98686b9defe7642bfc1475a1d6dafa624203eb74
eddybruv <edwinajong@gmail.com>
2022-07-05 21:45:55 +0100

    new nav option component

--- client/src/components/partials/Nav/Option.tsx
+++ client/src/components/partials/Nav/Option.tsx
@@ -1,14 +1,30 @@
-import React, {FC, ReactNode} from "react";
+import React, {FC, ReactNode, SetStateAction, useContext} from "react";
 import style from "./styles/option.module.css"
+import HomeContext from "../../../HomeContext";
+import Home from "../../../pages/Home"
 
 interface Props {
   icon: ReactNode;
   text: string;
+  component: SetStateAction<JSX.Element>;
 }
 
-const Options = ({icon, text}: Props) => {
+interface ContextType {
+  content?: JSX.Element;
+  setContent?:  React.Dispatch<React.SetStateAction<JSX.Element>>
+}
+
+const Options = ({icon, text, component}: Props) => {
+  let {content, setContent} = useContext(HomeContext) as ContextType;
+
+  const handleSwitch = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setContent && setContent(component);
+    console.log(content);
+  }
+
   return (
-    <div className={style.navBox}>
+    <div onClick={handleSwitch} className={style.navBox}>
       {icon}
       <p>{text}</p>
     </div>
