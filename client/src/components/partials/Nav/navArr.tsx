18db3715f673b122fcb7e919ac157bd19d438673
eddybruv <edwinajong@gmail.com>
2022-07-05 21:46:08 +0100

    array of nav options

--- client/src/components/partials/Nav/navArr.tsx
+++ client/src/components/partials/Nav/navArr.tsx
@@ -1,41 +1,51 @@
-import {ReactNode} from "react";
-import {HomeOutlined, SvgIconComponent} from "@mui/icons-material";
+import {ReactNode, FC, SetStateAction} from "react";
+import {HomeOutlined} from "@mui/icons-material";
 import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
 import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
 import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
+import Home from "../../../pages/Home";
+import Body from "../../Notes/Body";
+import DashboardBody from "../../Dashboard/DashboardBody"
 
 interface IOption {
-  icon: ReactNode,
+  icon: ReactNode;
   text: string;
+  component: SetStateAction<JSX.Element>;
 }
 
 const options: IOption[] = [
   {
-    icon: <HomeOutlined/> ,
-    text: "Dashboard"
+    icon: <HomeOutlined />,
+    text: "Dashboard",
+    component: <DashboardBody />,
   },
   {
-    icon: <EventNoteOutlinedIcon/> ,
-    text: "Notes"
+    icon: <EventNoteOutlinedIcon />,
+    text: "Notes",
+    component: <Body />,
   },
   {
-    icon: <PeopleAltOutlinedIcon/> ,
-    text: "Teams"
+    icon: <PeopleAltOutlinedIcon />,
+    text: "Teams",
+    component: <Body />,
   },
   {
-    icon: <CheckBoxOutlinedIcon/> ,
-    text: "Tasks"
+    icon: <CheckBoxOutlinedIcon />,
+    text: "Tasks",
+    component: <Body />,
   },
   {
-    icon: <SettingsOutlinedIcon/> ,
-    text: "Administration"
+    icon: <SettingsOutlinedIcon />,
+    text: "Administration",
+    component: <Body />,
   },
   {
-    icon: <ForumOutlinedIcon/> ,
-    text: "Chat"
+    icon: <ForumOutlinedIcon />,
+    text: "Chat",
+    component: <Body />,
   },
-]
+];
 
 export default options
