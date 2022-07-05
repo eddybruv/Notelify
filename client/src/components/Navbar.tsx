5a5e3919583ec1723c0b3d9b7c067def6722d446
eddybruv <edwinajong@gmail.com>
2022-07-05 21:47:26 +0100

    added attributes and props to component usage

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import navArr from "./partials/Nav/navArr";
 import {NotificationsOutlined} from "@mui/icons-material";
 import avatar from "../assets/runstar.jpg";
 import {Avatar} from "@mui/material";
+import Home from "../pages/Home";
 
 const Navbar: FC = () => {
   return (
@@ -27,12 +28,12 @@ const Navbar: FC = () => {
           </section>
           <section className={style.navSection}>
             {navArr.map((nav, index: number) => {
-              return <Options icon={nav.icon} text={nav.text} key={index} />;
+              return <Options component={nav.component} icon={nav.icon} text={nav.text} key={index} />;
             })}
           </section>
         </section>
         <section className={style.bottom}>
-          <Options icon={<NotificationsOutlined/>} text={"Notification"}/>
+          <Options component={<Home/>} icon={<NotificationsOutlined/>} text={"Notification"}/>
           <section className={style.personalInfo}>
             <Avatar src={avatar}/>
             <p>Profile</p>
