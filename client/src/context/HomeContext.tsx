d3367b0c0233490335e83f809e26d0933fd58615
eddybruv <edwinajong@gmail.com>
2022-07-06 21:36:12 +0100

    changed import path

--- client/src/context/HomeContext.tsx
+++ client/src/context/HomeContext.tsx
@@ -1,5 +1,5 @@
 import React, {createContext, useState, FC, ReactNode} from "react";
-import ViewNotes from "./components/Notes/Body";
+import ViewNotes from "../components/Notes/NotesBody";
 
 export interface IHomeContext {
   content?: JSX.Element;
