7e1f5ede754ec7d3d20cd0078d05db1097d32ca3
eddybruv <edwinajong@gmail.com>
2022-07-06 21:32:30 +0100

    dummy array of notes

---
+++ client/src/components/Notes/notesArr.tsx
@@ -0,0 +1,109 @@
+interface IOneNote {
+  imageUrl: string;
+  name: string;
+  createdAt: string;
+  header: string;
+  content: string;
+}
+
+
+const notes: IOneNote[] = [
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/80322163?v=4",
+    name: "Edwin Ajong",
+    header: "First Note",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+      "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
+      "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
+      "optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n" +
+      "obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n" +
+      "nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n" +
+      "tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n" +
+      "quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n" +
+      "sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n" +
+      "recusandae alias error harum maxime adipisci amet laborum. Perspiciatis ",
+    createdAt: "2022-06-27T14:31:52.818+00:00"
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/80322163?v=4",
+    name: "Edwin Ajong",
+    header: "First Note",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+      "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
+      "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
+      "optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n" +
+      "obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n" +
+      "nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n" +
+      "tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n" +
+      "quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n" +
+      "sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n" +
+      "recusandae alias error harum maxime adipisci amet laborum. Perspiciatis ",
+    createdAt: "2022-06-27T14:31:52.818+00:00"
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/80322163?v=4",
+    name: "Edwin Ajong",
+    header: "First Note",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+      "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
+      "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
+      "optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n" +
+      "obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n" +
+      "nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n" +
+      "tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n" +
+      "quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n" +
+      "sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n" +
+      "recusandae alias error harum maxime adipisci amet laborum. Perspiciatis ",
+    createdAt: "2022-06-27T14:31:52.818+00:00"
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/80322163?v=4",
+    name: "Edwin Ajong",
+    header: "First Note",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+      "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
+      "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
+      "optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n" +
+      "obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n" +
+      "nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n" +
+      "tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n" +
+      "quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n" +
+      "sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n" +
+      "recusandae alias error harum maxime adipisci amet laborum. Perspiciatis ",
+    createdAt: "2022-06-27T14:31:52.818+00:00"
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/80322163?v=4",
+    name: "Edwin Ajong",
+    header: "First Note",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+      "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
+      "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
+      "optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n" +
+      "obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n" +
+      "nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n" +
+      "tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n" +
+      "quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n" +
+      "sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n" +
+      "recusandae alias error harum maxime adipisci amet laborum. Perspiciatis ",
+    createdAt: "2022-06-27T14:31:52.818+00:00"
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/80322163?v=4",
+    name: "Edwin Ajong",
+    header: "First Note",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n" +
+      "molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n" +
+      "numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n" +
+      "optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n" +
+      "obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n" +
+      "nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,\n" +
+      "tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,\n" +
+      "quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n" +
+      "sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n" +
+      "recusandae alias error harum maxime adipisci amet laborum. Perspiciatis ",
+    createdAt: "2022-06-27T14:31:52.818+00:00"
+  }
+]
+
+export default notes
