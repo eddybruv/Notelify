e58aa219f14a23d4656415b8e15c1c77b36debd9
eddybruv <edwinajong@gmail.com>
2022-07-06 21:31:58 +0100

    moved context file, worked on the notes component

---
+++ client/src/components/Notes/NotesBody.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+import style from "../../styles/notesbody.module.css";
+
+const ViewNotes:FC = () => {
+  return (
+    <section className={style.body}>
+      <section className={style.header}>
+        me
+      </section>
+    </section>
+  )
+}
+
+export default ViewNotes
