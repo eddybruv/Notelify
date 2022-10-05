29106a322b83f1f18c23e47ddacb93368b160c53
Edwin Ajong <edwinajong@gmail.com>
2022-10-04 22:29:38 -0500

    included the navigate if user signed in function

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,5 +1,6 @@
-import React, {createContext, ReactNode, FC, useState} from "react";
-import {IUser} from '../types/user.types'
+import React, {createContext, ReactNode, FC, useState, useEffect, useContext} from "react";
+import {IUser} from '../types/user.types';
+import {useNavigate} from 'react-router-dom';
 
 interface IComponent {
   children?: ReactNode;
@@ -9,10 +10,26 @@ export const UserContext = createContext<IUser | {}>({});
 
 export const UserProvider: FC<IComponent> = ({children}: IComponent) => {
   const [user, setUser] = useState ({} as IUser);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const data = localStorage.getItem('userInfo');
+    const userInfo: IUser = data && JSON.parse(data);
+    setUser(userInfo);
+
+    if(!userInfo) {
+      navigate('/home')
+    }
+  
+  }, [navigate])
 
   return (
     <UserContext.Provider value={{user, setUser}}>
       {children}
     </UserContext.Provider>
   )
+}
+
+export const UserState = () => {
+  return useContext(UserContext)
 }
