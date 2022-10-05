115e5f0d3bfa57246e8361b8a36f057ac740b453
Edwin Ajong <edwinajong@gmail.com>
2022-10-04 22:21:45 -0500

    fixed user context problem

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,18 +1,14 @@
-import React, {createContext, Dispatch, ReactNode, SetStateAction, FC, useState} from "react";
-
-export interface IUserContext {
-  user?: null;
-  setUser?: Dispatch<SetStateAction<JSX.Element>>;
-}
+import React, {createContext, ReactNode, FC, useState} from "react";
+import {IUser} from '../types/user.types'
 
 interface IComponent {
   children?: ReactNode;
 }
 
-export const UserContext = createContext<IUserContext | null>(null);
+export const UserContext = createContext<IUser | {}>({});
 
 export const UserProvider: FC<IComponent> = ({children}: IComponent) => {
-  const [user, setUser] = useState<IUserContext | null>(null);
+  const [user, setUser] = useState ({} as IUser);
 
   return (
     <UserContext.Provider value={{user, setUser}}>
