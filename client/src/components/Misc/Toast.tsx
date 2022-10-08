d06e2be18258220c7dfa422dcd7af68857115664
Edwin Ajong <edwinajong@gmail.com>
2022-10-07 22:27:38 -0500

    created a reusable toast

---
+++ client/src/components/Misc/Toast.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Alert, Snackbar } from "@mui/material";
+
+interface Props {
+  message: string;
+  severity: "error" | "info" | "success";
+}
+
+const Toast = ({ message, severity }: Props) => {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+    >
+      <Alert
+        variant="filled"
+        onClose={handleClose}
+        sx={{ width: "100%" }}
+        severity={severity}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+export default Toast;
