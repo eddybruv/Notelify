eac26e414997254fdd8685fdb7f9e07d7b428548
Edwin Ajong <edwinajong@gmail.com>
2022-10-08 21:45:01 -0500

    wrapped user context around app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,47 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Login from "./pages/auth/Login"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 
-import {HomeProvider} from "./context/HomeContext";
+import { HomeProvider } from "./context/HomeContext";
+import { UserProvider } from "./context/UserContext";
 
-import {createTheme, ThemeProvider} from "@mui/material/styles";
-import './App.css';
-import ConfirmPassword from './pages/auth/ConfirmPassword';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./App.css";
+import ConfirmPassword from "./pages/auth/ConfirmPassword";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#0064fe"
+      main: "#0064fe",
     },
     secondary: {
-      main: "#89909c"
-    }
-  }
-})
+      main: "#89909c",
+    },
+  },
+});
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <HomeProvider>
-        <div className="App">
-          <Router>
-            <Routes>
-              <Route path={"/"} element={<Register/>}/>
-              <Route path={"/login"} element={<Login/>}/>
-              <Route path={"/home"} element={<Home/>}/>
-              <Route path={"/confirm-password"} element={<ConfirmPassword/>} />
-            </Routes>
-          </Router>
-        </div>
-      </HomeProvider>
+      <Router>
+        <UserProvider>
+          <HomeProvider>
+            <div className="App">
+              <Routes>
+                <Route path={"/"} element={<Register />} />
+                <Route path={"/login"} element={<Login />} />
+                <Route path={"/home"} element={<Home />} />
+                <Route
+                  path={"/confirm-password"}
+                  element={<ConfirmPassword />}
+                />
+              </Routes>
+            </div>
+          </HomeProvider>
+        </UserProvider>
+      </Router>
     </ThemeProvider>
   );
 }
